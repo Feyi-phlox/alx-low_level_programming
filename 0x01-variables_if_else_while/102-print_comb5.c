c365416d640a06e85f5b2e1a82c6eeacb1af6e96
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-20 04:06:35 -0700

    102-print_comb5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,7 +3,7 @@
 /**
  * main - prints all possible combinations of two two-digit numbers
  *
- * Return: o when unsuccessful
+ * Return: 0 when unsuccessful
  */
 
 int main(void)
@@ -11,16 +11,22 @@ int main(void)
 	int a;
 	int b;
 
-	for (a = 00 ; a <= 99 ; a++)
+	for (a = 0 ; a < 100 ; a++)
 	{
-		for (b = 01 ; b  <= 99 ; b++)
+		for (b = 0 ; b  < 100 ; b++)
 		{
-			if (a < b && a == b)
+			if (a < b)
 			{
-				putchar(a + '0');
-				putchar(b + '0');
-				putchar(',');
+				putchar((a / 10) + 48);
+				putchar((a % 10) + 48);
 				putchar(' ');
+				putchar((b / 10) + 48);
+				putchar((b % 10) + 48);
+				if (a != 98 || b != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
 		}
 	}
