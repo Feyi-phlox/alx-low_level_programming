05e97cfccbeec10faf8ce79d84f815f04ff0280f
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-21 15:33:38 -0700

    7-last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_last_digit - prints the last digit of a number
+ * @i: parameter that is printed
+ * Return: a
+ */
+
+int print_last_digit(int i)
+{
+	int a;
+
+	a = i % 10;
+	if (i < 0)
+		a = -a;
+	_putchar(a + '0');
+	return (a);
+}
