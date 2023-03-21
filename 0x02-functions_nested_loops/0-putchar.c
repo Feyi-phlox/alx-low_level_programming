71823be2dfb02d5ed209ca1cec14f90b4c406a3d
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-21 12:14:43 -0700

    0-putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - main fucntion
+ * _putchar: prints the alphabet putchar
+ * Return: 0 when unsuccessful
+ */
+
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
