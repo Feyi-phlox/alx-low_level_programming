0e7d9313feee89a7815f9cccc6dbea0656d4fb58
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-21 17:29:15 -0700

    10-add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns result
+ *@a: parameter 1
+ *@b: parameter 2
+ * Return: always sum
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
