9d2a3c4112449548b94d1408af1e39b4b8cec19b
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-22 13:40:32 -0700

    9-times_table

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even valued Fibonacci numbers to 4million
+ * Return: 0 when unsuccessful
+ */
+
+int main(void)
+{
+        int count = 3;
+
+        long int first = 1, second = 2;
+        long int next = first + second;
+
+        printf("%lu, ", first);
+        printf("%lu, ", second);
+        while (count <= 50)
+        {
+                if (count == 50)
+                {
+                        printf("%lu\n", next);
+                }
+                else
+                {
+                        printf("%lu, ", next);
+                }
+                first = second;
+                second = next;
+
+                next = first + second;
+		sum =
+		if (count <= 4000000, count ++)
+		{
+			print
+        }
+        return (0);
+}
