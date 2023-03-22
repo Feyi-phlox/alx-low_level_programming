17e68686fbdb407039c9e5c6e12da4aebf401d3d
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-22 16:10:24 -0700

    100-times_table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,49 @@
 #include "main.h"
 
 /**
- * print_times_table - prints the n times table starting from 0
- * @n: the number of times table to print
- *
- * Return: always 0
- */
-
+* print_times_table - prints the n times table, starting with 0
+* @n: the number of times table to print
+*
+* Return: void
+*/
 void print_times_table(int n)
 {
-	int a, b, c;
+int i, j, k;
 
-	if (n > 15 && n < 0)
-	return;
-	for (a = 0; a <= n; a++)
-	{
-		for (b = 1; b <= n; b++)
-		{
-			c = a * b;
-			if (b == 0)
-				_putchar(c + '0');
-			else if (c < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(c + '0');
-			}
-			else if (c < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(c / 10 + '0');
-				_putchar(c % 10 + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(c / 100 + '0');
-				_putchar((c / 10) % 10 + '0');
-				_putchar(c % 10 + '0');
-			}
-			_putchar('\n');
-		}
-	}
+if (n > 15 || n < 0)
+return;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+k = i * j;
+if (j == 0)
+_putchar(k + '0');
+else if (k < 10)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0');
+}
+else
+{
+_putchar(',');
+_putchar(' ');
+_putchar(k / 100 + '0');
+_putchar((k / 10) % 10 + '0');
+_putchar(k % 10 + '0');
+}
+}
+_putchar('\n');
+}
 }
