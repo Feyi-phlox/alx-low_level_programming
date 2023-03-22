cd35276bad50fdd76a8f560c54e14d40721757c1
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-22 07:02:00 -0700

    natural

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * print_times_table - prints the n times table starting from 0
+ *@:
+ * Return: always 0
+ */
+
+void print_times_table(int n)
---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints sum of all multiples of 3 or 5 below 1024
+ * Return: 0 when unsuccessful
+ */
+
+int main(void)
+{
+	int i;
+	int sum;
+
+	sum = 0;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+		if ((i % 3 == 0) || (i % 5 == 0))
+		{
+			sum = sum + i;
+		}
+	}
+	printf("%d\n", sum);
+	return (0);
+}
