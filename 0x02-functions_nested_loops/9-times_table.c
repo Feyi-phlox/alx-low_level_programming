6a54bb39c27914fef8ee0ad4ccc0b27212fefec3
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-22 17:37:23 -0700

    9-times_table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,29 +12,28 @@ void times_table(void)
 
 	for (a = 0; a < 10; a++)
 	{
-		for (b = 0; b < 10; b++)
-		{
-			c = a * b;
-			if (b == 0)
-			{
-				_putchar('0');
-			}
-			else if (c < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(c + '0');
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(c / 10 + '0');
-				_putchar(c % 10 + '0');
-			}
-			_putchar('\n');
-		}
+	for (b = 0; b < 10; b++)
+	{
+	c = a * b;
+	if (b == 0)
+	{
+	_putchar('0');
+	}
+	else if (c < 10)
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(' ');
+	_putchar(c + '0');
+	}
+	else
+	{
+	_putchar(',');
+	_putchar(' ');
+	_putchar(c / 10 + '0');
+	_putchar(c % 10 + '0');
+	}
+	_putchar('\n');
+	}
 	}
 }
-
