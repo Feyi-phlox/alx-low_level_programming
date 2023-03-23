59219acaa0e20b8709e66c039bc7bd5a97d4d7b9
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-23 01:43:10 -0700

    0-main

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - testing for positive or negative
+ *@i: parameter to be checked
+ * Return: void
+ *
+ */
+
+void positive_or_negative(int i)
+{
+	if (i < 0)
+		printf("%d is negative\n", i);
+	else
+		printf("%d is positive\n", i);
+}
