d5a923b8392c1361e1a1660b5f35d244aff85a5d
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-28 04:59:09 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,19 +2,21 @@
 
 /**
  * print_rev -prints a string in reverse followed by a new line
- * @str: parameter printed in reverse
+ * @s: parameter printed in reverse
  * Return: always 0
  */
 
-void print_rev(char *str)
+void print_rev(char *s)
 {
 	int i;
 	int count = 0;
 
-	for (i = 0 ; str[i] != '\0' ; i++)
+	for (i = 0 ; s[i] != '\0' ; i++)
 		count++;
 	
-	for (i = count ; i > 0; i--)
-		_putchar(str[i]);
+	count--;
+
+	for (i = count ; i >= 0; i--)
+		_putchar(s[i]);
 	_putchar('\n');
 }
