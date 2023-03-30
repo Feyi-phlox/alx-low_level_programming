15b634da7907c7d375168d81e993b112c632d88f
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-30 15:30:26 -0700

    1-strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -14,14 +14,12 @@ char *_strncat(char *dest, char *src, int n)
 	int srclen = 0;
 	int i;
 
-	for (i = 0 ; dest[i] != '\0' ; i++)
+	for (i = 0; dest[i] != '\0' ; i++)
 		destlen++;
 	for (i = 0 ; src[i] != '\0' ; i++)
 		srclen++;
-	while (i = 0 ; i < n && src[i] != '\0' ; i++)
-	{
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
 		dest[destlen + i] = src[i];
-		dest[destlen + i] = '\0';
-	}
+	dest[destlen + i] = '\0';
 	return (dest);
 }
