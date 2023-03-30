96c40e3aef33f12b0d843bde4cde557574004ceb
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-03-30 15:08:37 -0700

    2-strncpy

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strncat - concatenates two strings
+ * @dest: destination string
+ * @src: source string
+ * @n:number of bytes of src
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int destlen = 0;
+	int srclen = 0;
+	int i;
+
+	for (i = 0 ; dest[i] != '\0' ; i++)
+		destlen++;
+	for (i = 0 ; src[i] != '\0' ; i++)
+		srclen++;
+	while (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+		dest[destlen + i] = src[i];
+		dest[destlen + i] = '\0';
+	}
+	return (dest);
+}
