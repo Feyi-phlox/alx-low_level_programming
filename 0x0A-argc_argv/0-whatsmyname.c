a5defdc35b50035a7ea9b900b9e2e6ad1a69f89c
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-04-07 16:54:56 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - function that prints its name followed by a new line
+ * @argc: number of arguments passed to the program
+ * @argv: argument vector(one-dimensional array if string)
+ * Return: 0
+ */
+
+int main(__attribute__ ((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
