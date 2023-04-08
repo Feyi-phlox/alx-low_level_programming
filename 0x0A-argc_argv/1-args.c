ac5c54245d900495203822e667604f348936d33d
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-04-07 17:36:03 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - function that prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+
+int main(int argc, __attribute__((unused)) char *argv[])
+{
+	{
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
