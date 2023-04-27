baa84b5d2b117b53dab5f85ff78a422a62c34489
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-04-27 16:56:46 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+
+/**
+ * pre_main - function before the main function is executed.
+ *
+ * Return: void
+ */
+
+void pre_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
