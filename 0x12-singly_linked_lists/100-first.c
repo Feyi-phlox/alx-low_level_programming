356a3cbdef730bccdd0e7a12a07211aa40a7261f
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-04-27 17:07:15 -0700

    function executed before main function

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 
 /**
- * pre_main - function before the main function is executed.
+ * before_main - function before the main function is executed.
  *
  * Return: void
  */
 
-void pre_main(void)
+void __attribute__ ((constructor)) before_main(void)
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back!\n");
