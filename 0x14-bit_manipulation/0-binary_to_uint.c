9ed2705da28e7f5e1d3d77e875e55044cddf00b0
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-05-04 12:47:48 -0700

    my first commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,16 +13,23 @@ unsigned int binary_to_uint(const char *b)
 {
 	unsigned int convert = 0;
 	int index = 0;
-	int len = strlen(b);
 
+	if (b == NULL)
+	{
+		return (0);
+	}
 
-	for (; index < len; index++)
+	for (; b[index] != '\0'; index++)
 	{
 		if (b[index] == '1')
 		{
-			convert += 1 << (len - index - 1);
+			convert = (convert << 1) | 1;
 		}
-		else if (b[index] != '0' || b == NULL)
+		else if (b[index] == '0')
+		{
+			convert = (convert << 1) | 0;
+		}
+		else
 		{
 			return (0);
 		}
