62931b7f1765f9790b2083f4d6df9b969ab865c9
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-05-04 15:21:19 -0700

    task 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,26 +10,26 @@ void print_binary(unsigned long int n)
 {
 	int i = sizeof(unsigned int) * 8 - 1;
 	int no_zero = 0;
+	unsigned long int convert;
+	
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 
 	for (; i >= 0; i--)
 	{
-		if (n & (1u << i))
+		convert = 1 << i;
+		if ((n & convert) == convert)
 		{
-			no_zero = 1;
 			_putchar('1');
+			no_zero = 1;
 		}
-		else
+		else if (no_zero)
 		{
-			if (no_zero)
-			{
-				_putchar('0');
-			}
+			_putchar('0');
 		}
 	}
-	if (!no_zero)
-	{
-		_putchar('0');
-	}
-
-	/*printf("\n");*/
+	return;
 }
