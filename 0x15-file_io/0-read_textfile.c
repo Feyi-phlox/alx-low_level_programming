75591937ef7357b3f99b648b6893c787cc71aa58
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-05-08 15:12:20 -0700

    first commit with README

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * read_textfile - function that reads a text file
+ * and prints it to the POSIX standard output
+ * @letters: number of letters it to be read and printed
+ * @filename: pointer to filename
+ * Return: actual number of letters or 0 if filename == NULL and if write fails
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buffer;
+	ssize_t read_bytes, written_bytes;
+	
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+	{
+		return (0);
+	}
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+	{
+		close (fd);
+		return (0);
+	}
+	read_bytes = read(fd, buffer, letters);
+	if (read_bytes == -1)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
+	if (written_bytes == -1 || written_bytes != read_bytes)
+	{
+		free(buffer);
+		close(fd);
+		return (0);
+	}
+	free(buffer);
+	close(fd);
+	return (0);
+}
