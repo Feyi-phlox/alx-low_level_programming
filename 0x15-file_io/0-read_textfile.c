5740474426e621d2eb3bc1e9bf580b90bc9de976
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-05-08 15:49:18 -0700

    first commit with README

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -21,26 +21,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	}
 
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+
 	read_bytes = read(fd, buffer, letters);
-	if (read_bytes == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+
 	written_bytes = write(STDOUT_FILENO, buffer, read_bytes);
-	if (written_bytes == -1 || written_bytes != read_bytes)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+
 	free(buffer);
 	close(fd);
-	return (0);
+	return (written_bytes);
 }
