f4e6ad04f9aa5c601fb97924147f5176792769d9
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-10-09 06:31:53 -0700

    header file

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,13 @@
+#ifndef  SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
+
+/* mandatory tasks 0-6 */
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+
+#endif
