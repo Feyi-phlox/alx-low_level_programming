bb1e77d21327105d1c0684cd9478834911ada637
Feyi-phlox <feyisayoakinbobola@gmail.com>
2023-10-10 03:24:14 -0700

    linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -17,7 +17,7 @@ int linear_search(int *array, size_t size, int value)
 		return (-1);
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checled array[%lu] = [%d]\n", i, array[i]);
+		printf("Value checled array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
 			return (i);
